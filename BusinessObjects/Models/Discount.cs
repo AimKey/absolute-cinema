3273fa592e6f9059534c86016a83ec2adca414dd
NhatNguyen48 <127458643+Clownfool@users.noreply.github.com>
2025-07-22 15:18:30 +0700

    Add files via upload

---
+++ BusinessObjects/Models/Discount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Models
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+        public decimal Percentage { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
